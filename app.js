83a59923bb5c6f5f2b47e9209bb22718c41bb1bc
passchanger <163418334+passchanger@users.noreply.github.com>
2025-08-31 23:43:55 +0530

    initial files

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ store.on("error", ()=>{
 
 const sessionOptions = {
     store,
-    secret : process.env.SECRETcode,
+    secret : process.env.SECRET,
     resave : false,
     saveUninitialized : true,
     cookie : {
--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const passport = require("passport");
 const {saveRedirectUrl} = require("../middleware.js");
 const userController = require("../controllers/user.js")
 
-router.get("/signup",userController.renderSignUpForm)
+router.get("/signup", userController.renderSignUpForm)
 
 router.post("/signup", WrapAsync(userController.signUp)
 );
@@ -15,6 +15,6 @@ router.get("/login", userController.renderLoginForm);
 
 router.post("/login", saveRedirectUrl, passport.authenticate("local", { failureRedirect: '/login', failureFlash:     true}), userController.login);
 
-router.get("/logout",userController.logOut);
+router.get("/logout", userController.logOut);
 
 module.exports = router;
